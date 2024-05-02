059dd3902f6b246a15b61b659aadbd7e1884d4b7
Tyler Boyd <ty@tyboyd.dev>
2024-05-02 16:53:42 -0500

    getting some temp data

--- main.go
+++ main.go
@@ -1,17 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"fmt"
+	"io"
+	"math"
+	"net/http"
+	"os"
+
+	"github.com/charmbracelet/log"
+	"github.com/joho/godotenv"
 )
+
+type WeatherValues struct {
+	Temperature float64 `json:"temperature"`
+}
+
+type WeatherData struct {
+	Values WeatherValues `json:"values"`
+}
+
+type WeatherRes struct {
+	Data WeatherData `json:"data"`
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+}
 
 func main() {
 	day := flag.String("day", "today", "What day's weather do you want to see?")
 	flag.Parse()
 
 	if flag.NArg() == 0 {
-		fmt.Printf("Here is the weather for %v!\n", *day)
+		log.Printf("Here is the weather for %v!\n", *day)
+		getWeather()
 	} else if flag.Arg(0) == "day" {
-		fmt.Printf("Here is the weather for %v!\n", *day)
+		log.Printf("Here is the weather for %v!\n", *day)
+		getWeather()
 	}
+}
+
+func getWeather() {
+	apiKey := os.Getenv("TOMORROW_API_KEY")
+	url := "https://api.tomorrow.io/v4/weather/realtime?location=64109&apikey=" + apiKey
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("error in api call - %v", err.Error())
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var w WeatherRes
+	err = json.Unmarshal(body, &w)
+	if err != nil {
+		log.Fatalf("error parsing json response - %v", err.Error())
+	}
+
+	c := w.Data.Values.Temperature
+	f := math.Round((c * 1.8) + 32)
+
+	log.Printf("Temp: %v°F", f)
 }
